92bcdd91cae07d41bcf0dee32236efe4e2843c53
jm macapagal <jmdmacapagal@gmail.com>
2019-05-08 19:49:20 +0800

    added renderIdea feature

--- app.js
+++ app.js
@@ -20,7 +20,17 @@ const ideas = [
 
 ]
 
+const renderIdea = function (ideas) {
+    const ideaList = document.querySelector('.list')
+    ideaList.innerHTML = ''
+    ideas.forEach(idea => {
+        const li = document.createElement('li')
+        li.textContent = idea.title
+        ideaList.appendChild(li)
+    })
+}
 
+renderIdea(ideas)
 
 document.querySelector('#addForm').addEventListener('submit', e => {
     e.preventDefault()
