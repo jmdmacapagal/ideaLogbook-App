2c28677893f9ceda3fad4737cf2c2dd0fc92f511
jm macapagal <jmdmacapagal@gmail.com>
2019-05-09 20:41:21 +0800

    checked codes

--- functions/add.js
+++ functions/add.js
@@ -3,15 +3,15 @@ const ideaDescription = document.querySelector('#ideaDescription')
 const ideas = localStorageData()
 
 document.querySelector('#saveButton').addEventListener('click', (e) => {
-    const id = generateID()
-    e.preventDefault()
-    ideas.push({
-        id: id,
-        title: ideaTitle.value,
-        description: ideaDescription.value,
-        createdAt: generateTimestamp(),
-        updatedAt: generateTimestamp()
-    })
-    saveIdea(ideas)
-    location.assign(`../index.html`)
+        const id = generateID()
+        e.preventDefault()
+        ideas.push({
+            id: id,
+            title: ideaTitle.value,
+            description: ideaDescription.value,
+            createdAt: generateTimestamp(),
+            updatedAt: generateTimestamp()
+        })
+        saveIdea(ideas)
+        location.assign(`../index.html`)
 })
